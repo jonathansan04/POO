c1c191ceac2b20e9d673949c46a1590e96a687b5
jonathansan04 <tatansantos4@gmail.com>
2019-04-01 11:24:29 -0500

    Ejemplo ciclos

--- src/interfaz/Principal.java
+++ src/interfaz/Principal.java
@@ -4,13 +4,24 @@
  * and open the template in the editor.
  */
 package interfaz;
-
+ import logica.CapturaTeclado;
+ import logica.EjemploCondicional;
+ import logica.EjemploCiclo;
 /**
  *
  * @author Estudiantes
  */
 public class Principal {
     public static void  main(String[]args){
-        System.out.println("Hola mundo, mi primer programa en java ");
+        System.out.println("Ejemplo de captura ");
+        CapturaTeclado ct= new CapturaTeclado();
+        ct.operacion();
+        System.out.println("Ejemplo de condicional");
+        EjemploCondicional ec = new EjemploCondicional();
+        ec.operacion();
+        System.out.println("Ejemplo de ciclos");
+        EjemploCiclo el = new EjemploCiclo();
+        el.operacion();
+    
     }
 }
